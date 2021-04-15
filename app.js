26ff8fb6eae8f2b820e581e2ea22cd47e843517a
Manan Aggarwal <mananaggarwal2001@gmail.com>
2021-04-15 21:03:25 +0530

    MongoDB

--- app.js
+++ app.js
@@ -2,23 +2,57 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
 const _ = require('lodash');
-let objectArray = [];
+const mongoose = require('mongoose');
+
+mongoose.connect("mongodb://localhost:27017/blogDetailDB", { useUnifiedTopology: true });
+
+
+const mainDetailsSchema = mongoose.Schema({
+    blogName: {
+        type: String,
+        reqired: true
+    },
+    catergorySelection: {
+        type: String,
+        required: true
+    },
+    topicName: {
+        type: String,
+        required: true
+    },
+    blogDetails: {
+        type: String,
+        required: true
+    }
+});
+const blogModel = mongoose.model("bloginformation", mainDetailsSchema);
+
 
 function blogFunction(bloggerName, catergorySelection, topicName, blogDetails) {
-    const object = {
+    let newBlog = new blogModel({
         blogName: bloggerName,
         catergorySelection: catergorySelection,
         topicName: topicName,
         blogDetails: blogDetails
-    };
-    objectArray.push(object);
+    });
+    blogArray.push(newBlog);
+    newBlog.save(); // Saving in the blog model
 }
 
-app.use(bodyParser.urlencoded({ extended: true }))
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 app.set('view engine', 'ejs');
 app.get("/", function(req, res) {
-    res.render("index", { blogArray: objectArray });
+    blogModel.find({}, (err, results) => {
+        if (err) {
+            console.log(err);
+            res.redirect("/");
+        } else {
+            res.render("index", {
+                blogArray: results,
+            });
+        }
+    })
 });
 
 app.get("/successfulPage", function(req, res) {
@@ -34,42 +68,57 @@ app.post("/blogDetails", function(req, res) {
 
     blogFunction(bloggerName, catergorySelection, topicName, blogDetails);
     res.render("successfulPage");
-})
-app.get("/particularBlog/:UserHeading", (req, res) => {
-    const HeadingLink = _.lowerCase(req.params.UserHeading);
-    objectArray.forEach(element => {
-        const BlogHeading = _.lowerCase(element.topicName);
-        if (BlogHeading === HeadingLink) {
-            res.render("ParticularBlog", {
-                ParticularBlogHeading: element.topicName,
-                particularBlogParagraph: element.blogDetails
-
+});
+app.get("/particularBlog/:UserID", (req, res) => {
+    const HeadingLink = req.params.UserID;
+    blogModel.find({ _id: HeadingLink }, (err, results) => {
+        if (err) {
+            console.log(err);
+        } else {
+            results.forEach(resultElement => {
+                res.render("ParticularBlog", {
+                    ParticularBlogHeading: resultElement.topicName,
+                    particularBlogParagraph: resultElement.blogDetails
+                });
             });
         }
-
     });
-});
+})
+
 app.post("/searchForBlog", (req, res) => {
-    const blogName = _.lowerCase(req.body.blogHeadingName);
-    objectArray.forEach(element => {
-        const blogHeadingName = _.lowerCase(element.topicName);
-
-        if (blogHeadingName === blogName) {
-            res.render("ParticularBlog", {
-                ParticularBlogHeading: element.topicName,
-                particularBlogParagraph: element.blogDetails
-
-            });
+    const blogHeadingName = _.lowerCase(req.body.blogHeadingName);
+    blogModel.find({}, (err, results) => {
+        if (err) {
+            console.log(err);
+        } else {
+            results.forEach(resultElement => {
+                let blogTopicName = _.lowerCase(resultElement.topicName);
+                if (blogTopicName === blogHeadingName) {
+                    res.render("ParticularBlog", {
+                        ParticularBlogHeading: resultElement.topicName,
+                        particularBlogParagraph: resultElement.blogDetails
+                    });
+                }
+            })
         }
     })
-
 });
 app.post("/", (req, res) => {
     let catergoryVariable = req.body.catergory;
-    res.render('catergoryPage', {
-        blogArray: objectArray,
-        chosenCatergory: catergoryVariable
+    blogModel.find({}, (err, results) => {
+        if (err) {
+            console.log(err);
+        } else {
+
+            res.render('catergoryPage', {
+                blogArray: results,
+                chosenCatergory: catergoryVariable
+            });
+
+
+        }
     });
+
 });
 
 app.listen(process.env.PORT || 3000, function() {
