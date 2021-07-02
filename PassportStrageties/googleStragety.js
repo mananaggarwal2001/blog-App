81aa9b1c89099b661553ec4cdeae75ffee9a83b0
Manan Aggarwal <mananaggarwal2001@gmail.com>
2021-07-02 22:56:28 +0530

    Modification 15

--- PassportStrageties/googleStragety.js
+++ PassportStrageties/googleStragety.js
@@ -1,40 +1,40 @@
-require('dotenv').config();
-const googleStragety = require("passport-google-oauth").OAuth2Strategy;
-const passport = require('passport');
-const User = require('../models/model').AuthRoute;
+ require('dotenv').config();
+ const googleStragety = require("passport-google-oauth").OAuth2Strategy;
+ const passport = require('passport');
+ const User = require('../models/model').AuthRoute;
 
-passport.serializeUser((user, done) => {
-    console.log(user);
-    done(null, user);
-}); // for creating the user cookie
-
-
-passport.deserializeUser((user, done) => {
-    User.find({ id: user.id }).then((user) => {
-        done(null, user);
-    });
-});
-
-// for destroying the cookie  and retreaving the information
+ passport.serializeUser((user, done) => {
+     console.log(user);
+     done(null, user);
+ }); // for creating the user cookie
 
 
-passport.use(new googleStragety({
-    clientID: process.env.G_ClientID,
-    clientSecret: process.env.GClient_Secret,
-    callbackURL: "http://localhost:3000/google/blogVerification",
-}, (request, accessToken, refreshToken, profile, done) => {
-    User.findOne({ id: profile.id }).then((currentUser) => {
-        if (currentUser) {
-            done(null, currentUser);
-        } else {
-            new User({
-                username: profile.displayName,
-                id: profile.id,
-                imagePath: profile.photos[0].value,
-                email: profile.emails[0].value,
-            }).save().then((user) => {
-                done(null, user);
-            });
-        }
-    });
-}));
+ passport.deserializeUser((user, done) => {
+     User.find({ id: user.id }).then((user) => {
+         done(null, user);
+     });
+ });
+
+ // for destroying the cookie  and retreaving the information
+
+
+ passport.use(new googleStragety({
+     clientID: process.env.G_ClientID,
+     clientSecret: process.env.GClient_Secret,
+     callbackURL: "http://localhost:3000/google/blogVerification",
+ }, (request, accessToken, refreshToken, profile, done) => {
+     User.findOne({ id: profile.id }).then((currentUser) => {
+         if (currentUser) {
+             done(null, currentUser);
+         } else {
+             new User({
+                 username: profile.displayName,
+                 id: profile.id,
+                 imagePath: profile.photos[0].value,
+                 email: profile.emails[0].value,
+             }).save().then((user) => {
+                 done(null, user);
+             });
+         }
+     });
+ }));
