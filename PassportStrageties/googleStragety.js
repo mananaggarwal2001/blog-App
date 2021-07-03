4846625c56059ce108a3376e3447b7e6899fbe5c
Manan Aggarwal <mananaggarwal2001@gmail.com>
2021-07-03 14:45:15 +0530

    heroku

--- PassportStrageties/googleStragety.js
+++ PassportStrageties/googleStragety.js
@@ -2,38 +2,38 @@
  const passport = require('passport');
  const User = require('../models/model').AuthRoute;
  require('dotenv').config();
- passport.serializeUser((user, done) => {
-     console.log(user);
-     done(null, user);
- }); // for creating the user cookie
+ //  passport.serializeUser((user, done) => {
+ //      console.log(user);
+ //      done(null, user);
+ //  }); // for creating the user cookie
 
 
- passport.deserializeUser((user, done) => {
-     User.find({ id: user.id }).then((user) => {
-         done(null, user);
-     });
- });
+ //  passport.deserializeUser((user, done) => {
+ //      User.find({ id: user.id }).then((user) => {
+ //          done(null, user);
+ //      });
+ //  });
 
- // for destroying the cookie  and retreaving the information
+ //  // for destroying the cookie  and retreaving the information
 
 
- passport.use(new googleStragety({
-     clientID: process.env.G_clientID,
-     clientSecret: process.env.GClient_Secret,
-     callbackURL: "http://localhost:3000/google/blogVerification",
- }, (request, accessToken, refreshToken, profile, done) => {
-     User.findOne({ id: profile.id }).then((currentUser) => {
-         if (currentUser) {
-             done(null, currentUser);
-         } else {
-             new User({
-                 username: profile.displayName,
-                 id: profile.id,
-                 imagePath: profile.photos[0].value,
-                 email: profile.emails[0].value,
-             }).save().then((user) => {
-                 done(null, user);
-             });
-         }
-     });
- }));
+ //  passport.use(new googleStragety({
+ //      clientID: process.env.G_clientID,
+ //      clientSecret: process.env.GClient_Secret,
+ //      callbackURL: "http://localhost:3000/google/blogVerification",
+ //  }, (request, accessToken, refreshToken, profile, done) => {
+ //      User.findOne({ id: profile.id }).then((currentUser) => {
+ //          if (currentUser) {
+ //              done(null, currentUser);
+ //          } else {
+ //              new User({
+ //                  username: profile.displayName,
+ //                  id: profile.id,
+ //                  imagePath: profile.photos[0].value,
+ //                  email: profile.emails[0].value,
+ //              }).save().then((user) => {
+ //                  done(null, user);
+ //              });
+ //          }
+ //      });
+ //  }));
